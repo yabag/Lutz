ac3fb6937cd851ec1b4e4a6298c8c20d8e79dd1a
yabag <yabagratyan@mail.ru>
2023-04-06 07:56:56 +0300

    some changes

--- test.py
+++ test.py
@@ -1,38 +1,4 @@
-from timer2 import total, bestof, bestoftotal
+import timeit
 
-t = total(pow, 2, 1000)[0]
-print(t)
-
-t = total(pow, 2, 1000, _reps=1000)[0]
-print(t)
-
-t = total(pow, 2, 1000, _reps=1000000)[0]
-print(t)
-
-best = bestof(pow, 2, 100000)[0]
-print(best)
-
-best = bestof(pow, 2, 1000000, _reps=30)[0]
-print(best)
-
-bot = bestoftotal(str.upper, 'spam', _reps1=30, _reps=1000)
-print(bot)
-
-tib = bestof(total, str.upper, 'spam', _reps=30)
-print(tib)
-
-
-def spam(a, b, c, d): return a + b + c + d
-
-
-t1 = total(spam, 1, 2, c=3, d=4, _reps=1000)
-print(t1)
-
-best = bestof(spam, 1, 2, c=3, d=4, _reps=1000)
-print(best)
-
-bot = bestoftotal(spam, 1, 2, c=3, d=4, _reps1=1000, _reps=1000)
-print(bot)
-
-bot2 = bestoftotal(spam, *(1, 2), _reps1=1000, _reps=1000, **dict(c=3, d=4))
-print(bot2)
+timer = min(timeit.repeat(stmt="[x ** 2 for x in range(1000)]", number=1000, repeat=5))
+print(timer)

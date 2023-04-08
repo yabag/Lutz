a0ccb92ce0a4be56a736b5f5c11ae9d68cef2330
yabag <yabagratyan@mail.ru>
2023-04-08 07:58:11 +0300

    some changes

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import timeit
 
-timer = min(timeit.repeat(stmt="[x ** 2 for x in range(1000)]", number=1000, repeat=5))
-print(timer)
+
+t1 = min(timeit.repeat(number=10000, repeat=3, stmt="L = [1, 2, 3, 4, 5]\nfor i in range(len(L)): L[i] += 1"))
+print(t1)
+
+t2 = min(timeit.repeat(number=10000, repeat=3, stmt="L = [1, 2, 3, 4, 5]\ni=0\nwhile i < len(L):\n\tL[i] += 1\n\ti += 1"))
+print(t2)
+
+t3 = min(timeit.repeat(number=10000, repeat=3, stmt="L = [1, 2, 3, 4, 5]\nM = [x + 1 for x in L]"))
+print(t3)

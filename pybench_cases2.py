f545366cd5a67146e94726059ddc228f7d2f7224
yabag <yabagratyan@mail.ru>
2023-04-19 13:12:27 +0300

    created pybench2.py and added some changes

--- pybench_cases2.py
+++ pybench_cases2.py
@@ -13,6 +13,10 @@ pythons = [
     (0, '/usr/bin/python2.7'),
     (0, '/usr/bin/pypy')
 ]
+pythons += [
+    (1, '/usr/bin/python3'),
+    (0, '/usr/bin/pypy')
+]
 
 stmts = [
     (0, 0, "[x ** 2 for x in range(1000)]"),
@@ -22,24 +26,18 @@ stmts = [
     (0, 0, "s = 'spam' * 2500\nx = [s[i] for i in range(10000)]"),
     (0, 0, "s = '?'\nfor i in range(10000): s += '?'")
 ]
-
-tracecmd = '-t' in sys.argv
-pythons = pythons if '-a' in sys.argv else None
-pybench.runner(stmts, pythons, tracecmd)
-
-pythons += [
-    (1, '/usr/bin/python3'),
-    (0, '/usr/bin/pypy')
-]
-
 stmts += [
     (0, 0, "[ord(x) for x in 'spam' * 2500]"),
     (0, 0, "res=[]\nfor x in 'spam' * 2500: res.append(ord(x))"),
     (0, 0, "$listif3(map(ord, 'spam' * 2500))"),
-    (0, 0, "list(ord(x) for x in 'spam' * 2500))"),
-    {0, 0, "{x ** 2 for x in range(1000)}"},
-    {0, 0, "s=set()\nfor x in range(1000): s.add(x ** 2"},
-    {0, 0, "{x: x ** 2 for x in range(1000)}"},
+    (0, 0, "list(ord(x) for x in 'spam' * 2500)"),
+    (0, 0, "{x ** 2 for x in range(1000)}"),
+    (0, 0, "s=set()\nfor x in range(1000): s.add(x ** 2)"),
+    (0, 0, "{x: x ** 2 for x in range(1000)}"),
     (0, 0, "d={}\nfor x in range(1000): d[x] = x ** 2"),
     (1, 1, "len(str(2 ** 1000000))")
 ]
+
+tracecmd = '-t' in sys.argv
+pythons = pythons if '-a' in sys.argv else None
+pybench.runner(stmts, pythons, tracecmd)
